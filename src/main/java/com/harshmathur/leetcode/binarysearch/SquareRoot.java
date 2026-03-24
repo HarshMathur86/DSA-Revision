79cc4996f7f030b33f5a98b7b4dbb643319f2ba6
harshmathur86 <99mharsh@gmail.com>
2026-03-24 08:58:00 +0530

    Leetcode addition

---
+++ src/main/java/com/harshmathur/leetcode/binarysearch/SquareRoot.java
@@ -0,0 +1,37 @@
+package com.harshmathur.leetcode.binarysearch;
+
+public class SquareRoot {
+
+    static void main() {
+        IO.println(mySqrt(15));
+    }
+
+    //Floor of square root
+    public static int mySqrt(int x) {
+        if (x<2) {
+            return x;
+        }
+
+        int s = 1;
+        int e = x/2;
+
+        while(s<=e) {
+            int m = s + (e-s)/2;
+            //necessay for Integer overflow fix
+            long square = (long) m * m;
+
+            if(square == x) {
+                return m;
+            } else if(square > x) {
+                //Move to left sub-arr
+                e = m-1;
+            } else if(square < x) {
+                ///Move to right
+                s = m+1;
+            }
+
+        }
+        return e;
+    }
+
+}
